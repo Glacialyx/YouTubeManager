8159be8406c56a1027ab7986e6160efbe53879f6
Glacialyx <85521122+Glacialyx@users.noreply.github.com>
2022-04-27 15:30:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 import videomanager
 
-#code...
+videolink = "https://www.youtube.com/watch?v=5Ec47-RwFsM"
+outputpath = "C:/Users/sadee/Downloads"
+itag = 315 #itag 315 = 4k (in this case)
+downloadvideo(videolink, outputpath, itag)
