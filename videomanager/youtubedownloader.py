2da4a8d7da1c4ba3af6b3819635a89ac3087ab60
Glacialyx <85521122+Glacialyx@users.noreply.github.com>
2022-04-27 15:29:54 +0200

    Update youtubedownloader.py

--- videomanager/youtubedownloader.py
+++ videomanager/youtubedownloader.py
@@ -1,5 +1,15 @@
 from pytube import YouTube
 
+'''
+In pytube's cipher.py:
+change var_regex at line 30 to this:
+re.compile(r"^\$*\w+\W")
+change lines 272-273 (in function_patterns) to this:
+r'a\.[a-zA-Z]\s*&&\s*\([a-z]\s*=\s*a\.get\("n"\)\)\s*&&\s*'
+r'\([a-z]\s*=\s*([a-zA-Z0-9$]{2,3})(\[\d+\])?\([a-z]\)'
+change line 288 to this:
+nfunc=re.escape(function_match.group(1))),
+'''
 
 def downloadvideo(videolink, outputpath, itag=-1):
     yt = YouTube(videolink)
