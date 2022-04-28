1822e6909e416d1fdb5c2561f257bd2bb362ffcd
Glacialyx <85521122+Glacialyx@users.noreply.github.com>
2022-04-28 02:11:48 +0200

    Update videotoframes.py

--- videomanager/videotoframes.py
+++ videomanager/videotoframes.py
@@ -9,7 +9,7 @@ def videotoframes(videoname, newfoldername=None):
     os.mkdir(newfoldername)
     print(cv2.__version__) # current version is 3.1.0
     vidcap = cv2.VideoCapture(videoname) # video needs to be in the same directory
-    count = 0
+    count = 1
     while True:
         success, image = vidcap.read()
         if not success:
@@ -17,4 +17,4 @@ def videotoframes(videoname, newfoldername=None):
         # save frame as JPEG file
         cv2.imwrite(os.path.join(newfoldername, f"frame{count}.jpg"), image)
         count += 1
-    print("{} images are extacted in {}.".format(count, newfoldername))
+    print("{} images are extacted in {}.".format(count-1, newfoldername))
