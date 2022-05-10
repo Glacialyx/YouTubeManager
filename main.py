c613f66ad090ec8b6ae78ac7b7438616e7579d8d
Glacialyx <85521122+Glacialyx@users.noreply.github.com>
2022-05-10 18:36:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import youtubemanager
 
-videolink = "https://www.youtube.com/watch?v=5Ec47-RwFsM"
-outputpath = "C:/Users/sadee/Downloads"
+videolink = "https://www.youtube.com/watch?v=LXb3EKWsInQ"
+outputpath = "C:/Users/username/Downloads"
 itag = 315 #itag 315 = 4k (in this case)
 downloadoptions(videolink, outputpath)
