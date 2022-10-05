32b4ef1cae523e5d7c0f10dd5c4e747cd202d07b
Glacialyx <85521122+Glacialyx@users.noreply.github.com>
2022-10-05 12:54:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ import youtubemanager
 
 videolink = "https://www.youtube.com/watch?v=LXb3EKWsInQ"
 outputpath = "C:/Users/username/Downloads"
-itag = 315 #itag 315 = 4k (in this case)
-downloadoptions(videolink, outputpath)
+itag = 'best audio'  # (optional) itag can be "best audio", "best video", "select" or certain numbers (315 = 4k in this case)
+yt = downloadoptions(videolink, outputpath, itag)
+print(f'{yt.title} has finished downloading')
