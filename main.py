e94f2edaeb761374692dcf8d69a280be5e4e96a8
Glacialyx <85521122+Glacialyx@users.noreply.github.com>
2023-08-12 15:27:53 +0200

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-import youtubemanager
-
-videolink = "https://www.youtube.com/watch?v=LXb3EKWsInQ"
-outputpath = "C:/Users/username/Downloads"
-itag = 'best audio'  # (optional) itag can be "best audio", "best video", "select" or certain numbers (315 = 4k in this case)
-yt = downloadoptions(videolink, outputpath, itag)
-print(f'{yt.title} has finished downloading')
